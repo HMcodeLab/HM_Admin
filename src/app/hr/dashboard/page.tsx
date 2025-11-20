8186a55d0c186f45b0ad933ab921985a286b7b3e
Avinash kumar pandey <105911954+avinash-kr-pandey@users.noreply.github.com>
2025-11-20 18:21:27 +0530

    new code

--- src/app/hr/dashboard/page.tsx
+++ src/app/hr/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import { ChatsCard } from '@/app/(home)/_components/chats-card'
-import { FirstHeader } from '@/components/hr/FirstHeader'
+// import { FirstHeader } from '@/components/hr/FirstHeader'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <FirstHeader />
+      {/* <FirstHeader /> */}
       <ChatsCard />
     </div>
   )
